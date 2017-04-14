ebc905c85152eaf98165635d6a1307c3379c43e6
sagar chopada <chopada.sagar32@gmail.com>
2017-04-14 23:49:13 +0530

    base64 working

--- script.js
+++ script.js
@@ -1,60 +1,110 @@
 // Code goes here
-(function() {
+(function () {
   "use strict";
 
-  var app = angular.module("photoSelectDemo", ["ui.bootstrap", 'ngFileUpload', 'angularUtils.directives.dirPagination','imageCropper'
+  var app = angular.module("photoSelectDemo", ["ui.bootstrap", 'ngFileUpload', 'angularUtils.directives.dirPagination', 'imageCropper'
 
 
   ]);
 
   app.controller('myController', ['$scope', myController])
   app.directive("pictureUriFromPhotoIdDirective", [pictureUriFromPhotoIdDirective])
-app.directive("dimentionClass",  [dimentionClass]);
+  app.directive("dimentionClass", [dimentionClass]);
 
-app.filter("convertToSrc",function(){
-  return function(pin){
-    return pin[Object.keys(pin)[0]];
-  };
-});
-app.controller('mainController', ['$scope', function($scope) {
-        /* jshint validthis: true */
-        var vm = this;
+  app.filter("convertToSrc", function () {
+    return function (pin) {
+      var blob = pin[Object.keys(pin)[0]];
+      var reader = new window.FileReader();
 
-        // Some cropper options.
-        vm.imageUrl = 'images/unsplash_' + getRandomInt(1,7) + '.jpg';
-        vm.showControls = true;
-        vm.fit = false;
+      reader.readAsDataURL();
+      console.log(reader);
 
-        vm.myButtonLabels = {
-          rotateLeft: ' (rotate left) ',
-          rotateRight: ' (rotate right) ',
-          zoomIn: ' (zoomIn) ',
-          zoomOut: ' (zoomOut) ',
-          fit: ' (fit) ',
-          crop: ' [crop] '
-        };
+      reader.onloadend = function () {
+        base64data = reader.result;
+        console.log(base64data);
+        return base64data;
+      }
 
-        vm.updateResultImage = function(base64) {
-          vm.resultImage = base64;
-          $scope.$apply(); // Apply the changes.
-        };
 
-        // Cropper API available when image is ready.
-        //vm.cropperApi = function(cropperApi) {
-        //  cropperApi.zoom(3);
-        //  cropperApi.rotate(270);
-        //  cropperApi.fit();
-        //  vm.resultImage = cropperApi.crop();
-        //  $scope.$apply(); // Apply the changes.
-        //};
+    };
+  });
+  app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
 
-        /**
-         * Returns a random integer between min (inclusive) and max (inclusive)
-         */
-        function getRandomInt(min, max) {
-          return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-      }]);
+        element.bind('change', function () {
+          scope.$apply(function () {
+            modelSetter(scope, element[0].files[0]);
+          });
+        });
+      }
+    };
+  }]);
+  app.controller('mainController', ['$scope', function ($scope) {
+    /* jshint validthis: true */
+    var vm = this;
+    $scope.showImg = false;
+    $scope.chnageFile = function () {
+
+      var file = $scope.myFile;
+
+      console.log('file is ');
+      console.dir(file);
+      var reader = new FileReader();
+
+      reader.addEventListener("load", function () {
+        // preview.src = reader.result;
+        $scope.imgBase64 = reader.result;
+        $scope.$apply(function () {
+          $scope.showImg = true;
+        });
+        alert($scope.imgBase64);
+      }, false);
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+
+    };
+
+    // Some cropper options.
+    vm.imageUrl = 'images/unsplash_' + getRandomInt(1, 7) + '.jpg';
+    vm.showControls = true;
+    vm.fit = false;
+
+    vm.myButtonLabels = {
+      rotateLeft: ' (rotate left) ',
+      rotateRight: ' (rotate right) ',
+      zoomIn: ' (zoomIn) ',
+      zoomOut: ' (zoomOut) ',
+      fit: ' (fit) ',
+      crop: ' [crop] '
+    };
+
+    vm.updateResultImage = function (base64) {
+      vm.resultImage = base64;
+      $scope.$apply(); // Apply the changes.
+    };
+
+    // Cropper API available when image is ready.
+    //vm.cropperApi = function(cropperApi) {
+    //  cropperApi.zoom(3);
+    //  cropperApi.rotate(270);
+    //  cropperApi.fit();
+    //  vm.resultImage = cropperApi.crop();
+    //  $scope.$apply(); // Apply the changes.
+    //};
+
+    /**
+     * Returns a random integer between min (inclusive) and max (inclusive)
+     */
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }]);
   function myController($scope) {
     $scope.test = '1d23'
 
@@ -66,8 +116,8 @@ app.controller('mainController', ['$scope', function($scope) {
         pictureUriFromPhotoIdDirective: "="
       },
       restrict: "A",
-      link: function($scope, $elem, $attr) {
-        $scope.$watch('pictureUriFromPhotoIdDirective', function(newVal) {
+      link: function ($scope, $elem, $attr) {
+        $scope.$watch('pictureUriFromPhotoIdDirective', function (newVal) {
           var uri;
           if (newVal != null) {
 
@@ -90,9 +140,9 @@ app.controller('mainController', ['$scope', function($scope) {
 
   function dimentionClass() {
     return {
-      link: function(scope, element) {
+      link: function (scope, element) {
         var img = element[0];
-        img.onload = function() {
+        img.onload = function () {
           var container = img.parentNode;
 
           var cwidth = parseInt(container.offsetWidth);
